ce5aa3c6215cf26e7c9c2be8d5e813256a3b0a3f
DigKleppe <Digkleppe@gmail.com>
2025-04-07 20:40:22 +0200

    accesspoint added

--- main/main.cpp
+++ main/main.cpp
@@ -10,21 +10,25 @@
 #include "freertos/task.h"
 #include "freertos/timers.h"
 
+#include "nvs_flash.h"
 #include "ADCTask.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "keys.h"
 #include "pompbeveiliging.h"
+#include "softAP.h"
+#include "udp.h"
 
 static const char *TAG = "main";
+static const int UDPPORT = 5000;
 
 myKey_t getKeyPins(void) { return (myKey_t)gpio_get_level(KEY_PIN); }
 
 void keysTimerHandler(TimerHandle_t xTimer) { keysTimerHandler_ms(10); }
 
 lampState_t lampState = LAMP_OFF;
-bool buzzerOn = false;
 state_t state = NORMAL;
+bool buzzerOn = false;
 
 void testTask(void *pvParameter) {
 	while (true) {
@@ -35,7 +39,7 @@ void testTask(void *pvParameter) {
 
 void testHardwareTask(void *pvParameter) {
 	bool stop = false;
-	ESP_LOGI(TAG, "HardwareTest");
+	ESP_LOGI("TASK", "HardwareTest");
 	while (!stop) {
 		gpio_set_level(LAMP_PIN, 1);
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
@@ -61,12 +65,13 @@ void testHardwareTask(void *pvParameter) {
 }
 
 void lampTask(void *pvParameter) {
-	ESP_LOGI(TAG, "LampTask");
+	ESP_LOGI("TASK", "LampTask");
 	while (true) {
 		switch (lampState) {
 		case LAMP_OFF:
 			gpio_set_level(LAMP_PIN, 0);
 			break;
+
 		case LAMP_ON:
 			gpio_set_level(LAMP_PIN, 1);
 			vTaskDelay(100 / portTICK_PERIOD_MS);
@@ -75,13 +80,13 @@ void lampTask(void *pvParameter) {
 			gpio_set_level(LAMP_PIN, 1);
 			vTaskDelay(10 / portTICK_PERIOD_MS);
 			gpio_set_level(LAMP_PIN, 0);
-			vTaskDelay(2000 / portTICK_PERIOD_MS);
+			vTaskDelay(100 / portTICK_PERIOD_MS);
 			break;
 		case LAMP_FLASH_SLOW:
 			gpio_set_level(LAMP_PIN, 1);
 			vTaskDelay(10 / portTICK_PERIOD_MS);
 			gpio_set_level(LAMP_PIN, 0);
-			vTaskDelay(10000 / portTICK_PERIOD_MS);
+			vTaskDelay(1000 / portTICK_PERIOD_MS);
 			break;
 		case LAMP_BLINK:
 			gpio_set_level(LAMP_PIN, 1);
@@ -115,75 +120,44 @@ void buzzerTask(void *pvParameter) {
 void mainTask(void *pvParameter) {
 
 	ESP_LOGI("TASK", "mainTask");
-	bool mainsPresent = false;
-	bool batteryLow = false;
 
 	while (true) {
 		if (ADCValue[ADC_5V] < ERRORLEVEL_5V) {
-			ESP_LOGI(TAG, "No mains voltage %2.1f", ADCValue[ADC_5V]);
-			mainsPresent = false;
+		//	ESP_LOGI("TASK", "No mains voltage");
 			if (state == NORMAL)
 				state = NOMAINS;
 		} else {
-			mainsPresent = true;
 			if (state == NOMAINS)
 				state = NORMAL;
 		}
 		if (ADCValue[ADC_BATT] < BATLOWLEVEL) {
-			ESP_LOGI(TAG, "Low bat %2.1f", ADCValue[ADC_BATT]);
-			batteryLow = true;
-		} else
-			batteryLow = false;
-
+		//	ESP_LOGI("TASK", "Low bat %2.2f", ADCValue[ADC_BATT]);
+			if (state == NOMAINS)
+				state = BATTERYLOW;
+		} else {
+			if (state == BATTERYLOW)
+				state = NORMAL;
+		}
 		if (ADCValue[ADC_CURRENT] > PUMPONLEVEL) {
-			ESP_LOGI(TAG, "Pump on %2.2f", ADCValue[ADC_CURRENT]);
+		//	ESP_LOGI("TASK", "Pump on %2.2f", ADCValue[ADC_CURRENT]);
 			if (state != PUMPWASACTIVEACCEPTED)
 				state = PUMPWASACTIVE;
-		}
-
-		if (mainsPresent)
-			gpio_set_level(GREEN_LED_PIN, 1);
-		else
-			gpio_set_level(GREEN_LED_PIN, 0);
-
-		if (batteryLow) {
-			if (state < PUMPWASACTIVE) {
-				gpio_set_level(RED_LED_PIN, 1);
-				vTaskDelay(10 / portTICK_PERIOD_MS);
-				gpio_set_level(RED_LED_PIN, 0);
-				vTaskDelay(1000 / portTICK_PERIOD_MS);
-			}
 		}
 		switch (state) {
 		case NORMAL:
 			lampState = LAMP_FLASH_SLOW;
-			gpio_set_level(GREEN_LED_PIN, 1);
-			gpio_set_level(RED_LED_PIN, 0);
-			gpio_set_level(KEY_LED_PIN, 0);
-
 			buzzerOn = false;
 			break;
 		case NOMAINS:
-			gpio_set_level(GREEN_LED_PIN, 0);
-			gpio_set_level(RED_LED_PIN, 1);
-
 			lampState = LAMP_FLASH_FAST;
 			buzzerOn = false;
 			break;
 		case BATTERYLOW:
-
-			gpio_set_level(RED_LED_PIN, 1);
-			vTaskDelay(10 / portTICK_PERIOD_MS);
-			gpio_set_level(RED_LED_PIN, 0);
-			vTaskDelay(1000 / portTICK_PERIOD_MS);
-
 			lampState = LAMP_OFF;
 			buzzerOn = false;
 			break;
 		case PUMPWASACTIVE:
-			gpio_set_level(RED_LED_PIN, 1);
 			lampState = LAMP_BLINK;
-			gpio_set_level(KEY_LED_PIN, 1);
 			buzzerOn = true;
 			if (key(KEY)) {
 				buzzerOn = false;
@@ -192,12 +166,6 @@ void mainTask(void *pvParameter) {
 			break;
 
 		case PUMPWASACTIVEACCEPTED:
-			gpio_set_level(RED_LED_PIN, 1);
-			gpio_set_level(KEY_LED_PIN, 1);
-			vTaskDelay(100 / portTICK_PERIOD_MS);
-			gpio_set_level(KEY_LED_PIN, 0);
-			vTaskDelay(100 / portTICK_PERIOD_MS);
-
 			lampState = LAMP_BLINK;
 			if (key(KEY)) {
 				state = NORMAL;
@@ -208,6 +176,25 @@ void mainTask(void *pvParameter) {
 			break;
 		}
 		vTaskDelay(10 / portTICK_PERIOD_MS);
+	}
+}
+
+void udpTask (void *pvParameter) {
+	ESP_LOGI("TASK", "udpTask");
+	char *buf = (char *)malloc(100);
+	int cnts = 0;
+	if (buf == NULL) {
+		ESP_LOGE(TAG, "Failed to allocate memory for UDP buffer");
+		vTaskDelete(NULL);
+		return;
+	}
+	int len = 0;
+	while (true) {
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		cnts++;
+		len = sprintf(buf, "C:%2.1fA 5V:%2.1fV Bat:%2.1fV State:%d hr:%d\n\r", ADCValue[ADC_CURRENT], ADCValue[ADC_5V], ADCValue[ADC_BATT], (int)state, cnts/(3600));
+		udpSend(buf, UDPPORT);
+		ESP_LOGI(TAG, "UDP: %s", buf);
 	}
 }
 extern "C" {
@@ -246,7 +233,6 @@ void app_main(void) {
 	gpio_set_level(GREEN_LED_PIN, 0);
 	gpio_set_level(RED_LED_PIN, 0);
 	gpio_set_level(LAMP_PIN, 0);
-	gpio_set_level(KEY_LED_PIN, 0);
 
 	gpio_set_direction(KEY_PIN, GPIO_MODE_INPUT);
 	keyTimer = xTimerCreate("keysTimer", 10, pdTRUE, NULL, keysTimerHandler);
@@ -258,11 +244,22 @@ void app_main(void) {
 
 	xTaskCreate(&lampTask, "lampTask", 1024 * 2, NULL, 0, NULL);
 	xTaskCreate(&buzzerTask, "buzzerTask", 1024 * 2, NULL, 0, NULL);
+	xTaskCreate(&udpTask, "udpTask", 1024 * 4, NULL, 0, NULL);
 	xTaskCreate(&mainTask, "mainTask", 1024 * 2, NULL, 0, NULL);
 
+	// Initialize NVS
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+
+	ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
+	wifi_init_softap();
+
 	while (true) {
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
-		ESP_LOGI(TAG, "state: %d", (int)state);
+		vTaskDelay(100 / portTICK_PERIOD_MS);
 	}
 }
 }
